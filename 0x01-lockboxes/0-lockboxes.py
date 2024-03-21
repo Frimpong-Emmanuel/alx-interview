da227b796cca11afd7c808546fbfaa285264a013
Frimpong-Emmanuel <emmanuelfrimpong426@gmail.com>
2024-03-21 19:09:19 +0000

    This is lock boxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Method to determine if all boxes can be opened 
+Using prototype: def canUnlockAll(boxes)
+"""
+
+
+def CanUnlockAll(boxes):
+    """
+    Check if boxes can be unlocked
+    """
+    for key in range(1, len(boxes) -1):
+        ctr = False
+        for idx in range(len(boxes)):
+            ctr = (key in boxes[idx] and key != idx)
+            if ctr:
+                break
+        if ctr is False:
+            return ctr
+    return True
+
+
+boxes = [[1], [2], [3], [4], []]
+print(CanUnlockAll(boxes))
+
+
+boxes = [[1, 4, 6],[2], [0, 4, 1],  [5, 6, 2], [3], [4, 1], [6]]
+print(CanUnlockAll(boxes))
+
+
+boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+print(CanUnlockAll(boxes))
