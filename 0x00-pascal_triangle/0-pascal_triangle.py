8078673e2b96e5a451396a46da0276c89ce97313
Frimpong-Emmanuel <emmanuelfrimpong426@gmail.com>
2024-03-21 19:23:08 +0000

    This is to make corrections

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -23,3 +23,14 @@ def pascal_triangle(n):
         temp.append(1)
         k.append(temp)
     return k
+
+def print_triagle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+
+    if __name__ == "__main__":
+        print_triangle(pascal_triangle(5))
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,1 @@
+This directory is about printing the pascal triangle
