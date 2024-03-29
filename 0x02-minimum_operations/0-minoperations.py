bc9479d8b6f566186b572e9c92dade8e9cd35450
Frimpong-Emmanuel <emmanuelfrimpong426@gmail.com>
2024-03-29 16:49:44 +0000

    Corrections

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""
-Minimum Operations
-"""
+""" Minimum Operations
+    """
 
 
 def minOperations(n: int) -> int:
     """
-    Minimum Operations needed to get n H characters
-    """
+    Minimum Operations needed to get n H characters"""
     next = 'H'
     body = 'H'
     op = 0
